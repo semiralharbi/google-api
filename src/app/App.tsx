dcccd4cdd1388ec943f5c1b72471bac4e4b6b9f4
semiiralharbi <semir.alharbi@gmail.com>
2023-03-25 09:13:43 +0100

    prevent zoom on mobile

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -17,6 +17,9 @@ function App() {
   return (
     <ThemeProvider theme={themeMode}>
       <GlobalStyle />
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+      </head>
       <main>
         <SwitchMode themeToggler={themeToggler} theme={theme} />
         <DivSquare id="map">
